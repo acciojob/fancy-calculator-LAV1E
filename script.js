d08ab4e8d9b9e7d0aacd6ed4363898fe3e3bfdfc
Anshika Bundela <99082647+LAV1E@users.noreply.github.com>
2023-12-10 12:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let firstVariable;
+let secondVariable;
+firstVariable =  Number(prompt("Enter a number"));
+firstVariable = firstVariable * 2;.
+
+secondVariable =  Number(prompt("Enter a number"));
+let result = firstVariable + secondVariable;
+alert("The final result is: " + result);
